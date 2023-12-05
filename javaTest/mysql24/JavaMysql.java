0f401ce22ac84c6e0321e987cee1e3ca53a7b31c
dreamtank77 <315001597@qq.com>
2023-12-05 14:23:18 +0900

    12-5

--- javaTest/mysql24/JavaMysql.java
+++ javaTest/mysql24/JavaMysql.java
@@ -1,9 +1,13 @@
 package com.javaTest.mysql24;
 
+import com.mysql.jdbc.Driver;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
+
 /**
  * @author
  * @version 1.0
@@ -13,12 +17,18 @@ import java.sql.Statement;
 public class JavaMysql {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         //加载类，获取mysql链接Class.forName("com.mysql.cj.jdbc.Driver");
-        Class.forName("com.mysql.jdbc.Driver");
+        //Class.forName("com.mysql.jdbc.Driver");
+        Driver driver = new Driver();
         //jdbc:mysql://localhost:3306/mydb?serverTimezone=UTC&useSSL=false
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "321321321");
+        String url = "jdbc:mysql://localhost:3306/tmp";
+        Properties properties = new Properties();
+        properties.setProperty("user", "root");
+        properties.setProperty("password", "321321321");
+
+        Connection connection = DriverManager.getConnection(url, properties);
 
         //String sql = "create table goods(id int, name varchar(32), price double, introduce text)";
-        String sql = "INSERT INTO goods(name)  values('tom3');";
+        String sql = "INSERT INTO mytable(name)  values('tom4');";
         Statement statement = connection.createStatement();
         statement.executeUpdate(sql);
 
